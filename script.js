881e47338fb16155a47ef663fc2776f84ddbf471
AnaliaRivarola <80989474+AnaliaRivarola@users.noreply.github.com>
2023-09-13 18:13:33 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,72 @@
+let intentos = 6;
+let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH'];
+const palabra = random(diccionario);
+
+window.addEventListener('load', init);
+
+function init() {
+    console.log('Esto se ejecuta solo cuando se carga la página web');
+}
+
+const button = document.getElementById("guess-button");
+button.addEventListener("click", intentar);
+
+function intentar() {
+    if (intentos > 0) {
+        const GRID = document.getElementById("grid");
+        const ROW = document.createElement('div');
+        ROW.className = 'row';
+
+        const INTENTO = leerIntento(); // Obtener el intento del usuario
+        for (let i = 0; i < palabra.length; i++) {
+            const SPAN = document.createElement('span');
+            SPAN.className = 'letter';
+            if (INTENTO[i] === palabra[i]) { // VERDE
+                SPAN.innerHTML = INTENTO[i];
+                SPAN.style.backgroundColor = '#79b851';
+            } else if (palabra.includes(INTENTO[i])) { // AMARILLO
+                SPAN.innerHTML = INTENTO[i];
+                SPAN.style.backgroundColor = '#f3c237';
+            } else { // GRIS
+                SPAN.innerHTML = INTENTO[i];
+                SPAN.style.backgroundColor = '#a4aec4';
+            }
+            ROW.appendChild(SPAN);
+        }
+        GRID.appendChild(ROW);
+
+        const valor = document.getElementById("guess-input").value.toUpperCase();
+        if (valor === palabra) {
+            terminar("¡Adivinaste la palabra!");
+        } else {
+            intentos--;
+            document.getElementById('remaining-guesses').textContent = `Intentos restantes: ${intentos}`;
+            if (intentos === 0) {
+                terminar(`¡Se acabaron los intentos! La palabra era "${palabra}".`);
+            }
+        }
+    }
+}
+
+function leerIntento() {
+    let intento = document.getElementById("guess-input");
+    intento = intento.value;
+    intento = intento.toUpperCase();
+    return intento;
+}
+
+function terminar(mensaje) {
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    button.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
+
+function random(diccionario) {
+    const indiceAleatorio = Math.floor(Math.random() * diccionario.length);
+    return diccionario[indiceAleatorio];
+}
+
+
+document.getElementById('remaining-guesses').textContent = `Intentos restantes: ${intentos}`;
